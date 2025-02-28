6d87cf34b423787fdb8953dd53315f8e5d65b167
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2025-03-01 00:25:17 +0700

    update

--- variables/main.swift
+++ variables/main.swift
@@ -1,2 +1,32 @@
-var str = "Hello, playground"
-print(str)
+import Foundation
+
+//declaring a variable
+let n1 = 10 //constant
+var n2 = 20
+
+//declaring multiple variables
+let n3 = 20, n4 = 30 //constant
+var n5 = 40, n6 = 50
+
+//<-------------------------------------->
+
+// var varialbe_name: data_type = value
+
+//declaring a variable with type
+let n7: Int = 10 //constant
+var n8: Int = 20
+var n9: Double = 30.0
+var n10: String = "Hello World"
+
+var n20: Int = 10, n21: Int = 20
+var n22: Double = 30.0, n23: Double = 40.0
+
+//<-------------------------------------->
+
+//printing the variables
+
+print("Variable n1: \(n1)")
+print("Variable n2: \(n2)")
+
+//<-------------------------------------->
+
