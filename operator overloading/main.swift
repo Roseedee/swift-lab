833e11a7ebb943a6cec8d8f8f94169ce303fd27d
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2025-03-01 23:02:50 +0700

    operator overloading

---
+++ operator overloading/main.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+print("Operator Overloading in Swift")
+
+struct Vector2D {
+    var x, y: Double
+
+    static func + (left: Vector2D, right: Vector2D) -> Vector2D {
+        return Vector2D(x: left.x + right.x, y: left.y + right.y)
+    }
+}
+
+let v1 = Vector2D(x: 1.0, y: 2.0)
+let v2 = Vector2D(x: 3.0, y: 4.0)
+
+let vs = v1 + v2
+
+print("vs.x: \(vs.x), vs.y: \(vs.y)")
